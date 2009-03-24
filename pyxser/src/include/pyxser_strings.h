699c013b7d7c6bca4e6a1fb82bb927f2a3634ed1
Daniel Molina Wegener <dmw@coder.cl>
2009-03-24 00:16:58 +0000

    - changed indentation.

--- pyxser/src/include/pyxser_strings.h
+++ pyxser/src/include/pyxser_strings.h
@@ -1,28 +1,28 @@
-/* -*- mode: c; indent-tabs-mode: t; tab-width: 4; c-file-style: "python" -*- */
+/* -*- mode: c; indent-tabs-mode: nil; tab-width: 4; c-file-style: "python" -*- */
 /* vim:set ft=c ff=unix ts=4 sw=4 enc=latin1 noexpandtab: */
 /* kate: space-indent off; indent-width 4; mixedindent off; indent-mode cstyle; */
 /*
-    Copyright (c) 2009 Daniel Molina Wegener <dmw@coder.cl>
+  Copyright (c) 2009 Daniel Molina Wegener <dmw@coder.cl>
 
-    This file is part of pyxser.
+  This file is part of pyxser.
 
-    pyxser is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
+  pyxser is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
 
-    pyxser is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  pyxser is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with pyxser.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with pyxser.  If not, see <http://www.gnu.org/licenses/>.
 
-    <!DOCTYPE pyxs:obj
-              PUBLIC "-//coder.cl//DTD pyxser 1.0//EN"
-              "http://projects.coder.cl/pyxser/dtd/pyxser-1.0.dtd">
- */
+  <!DOCTYPE pyxs:obj
+  PUBLIC "-//coder.cl//DTD pyxser 1.0//EN"
+  "http://projects.coder.cl/pyxser/dtd/pyxser-1.0.dtd">
+*/
 
 #ifndef PYXSER_STRINGS_H
 #define PYXSER_STRINGS_H 1
@@ -33,26 +33,26 @@
 extern "C" {
 #endif /* !__cplusplus */
 
-xmlNodePtr pyxser_SerializeString(PyObject *o, char *name,
-								  PyListObject *dupItems, xmlDocPtr doc);
+    xmlNodePtr pyxser_SerializeString(PyObject *o, char *name,
+                                      PyListObject *dupItems, xmlDocPtr doc);
 
-xmlNodePtr pyxser_SerializeExactString(PyObject *o, char *name,
-									   PyListObject *dupItems, xmlDocPtr doc);
+    xmlNodePtr pyxser_SerializeExactString(PyObject *o, char *name,
+                                           PyListObject *dupItems, xmlDocPtr doc);
 
-xmlNodePtr pyxser_SerializeUnicode(PyObject *o, char *name,
-								   PyListObject *dupItems, xmlDocPtr doc);
+    xmlNodePtr pyxser_SerializeUnicode(PyObject *o, char *name,
+                                       PyListObject *dupItems, xmlDocPtr doc);
 
-xmlNodePtr pyxser_SerializeExactUnicode(PyObject *o, char *name,
-										PyListObject *dupItems, xmlDocPtr doc);
+    xmlNodePtr pyxser_SerializeExactUnicode(PyObject *o, char *name,
+                                            PyListObject *dupItems, xmlDocPtr doc);
 
-xmlNodePtr pyxser_SerializeBuffer(PyObject *o, char *name,
-								  PyListObject *dupItems, xmlDocPtr doc);
+    xmlNodePtr pyxser_SerializeBuffer(PyObject *o, char *name,
+                                      PyListObject *dupItems, xmlDocPtr doc);
 
-PyObject *pyxunser_SerializeString(PythonUnserializationArgumentsPtr obj);
-PyObject *pyxunser_SerializeExactString(PythonUnserializationArgumentsPtr obj);
-PyObject *pyxunser_SerializeUnicode(PythonUnserializationArgumentsPtr obj);
-PyObject *pyxunser_SerializeExactUnicode(PythonUnserializationArgumentsPtr obj);
-PyObject *pyxunser_SerializeBuffer(PythonUnserializationArgumentsPtr obj);
+    PyObject *pyxunser_SerializeString(PythonUnserializationArgumentsPtr obj);
+    PyObject *pyxunser_SerializeExactString(PythonUnserializationArgumentsPtr obj);
+    PyObject *pyxunser_SerializeUnicode(PythonUnserializationArgumentsPtr obj);
+    PyObject *pyxunser_SerializeExactUnicode(PythonUnserializationArgumentsPtr obj);
+    PyObject *pyxunser_SerializeBuffer(PythonUnserializationArgumentsPtr obj);
 
 #ifdef __cplusplus
 };
--- pyxser/src/include/pyxser_tools.h
+++ pyxser/src/include/pyxser_tools.h
@@ -1,28 +1,28 @@
-/* -*- mode: c; indent-tabs-mode: t; tab-width: 4; c-file-style: "python" -*- */
+/* -*- mode: c; indent-tabs-mode: nil; tab-width: 4; c-file-style: "python" -*- */
 /* vim:set ft=c ff=unix ts=4 sw=4 enc=latin1 noexpandtab: */
 /* kate: space-indent off; indent-width 4; mixedindent off; indent-mode cstyle; */
 /*
-    Copyright (c) 2009 Daniel Molina Wegener <dmw@coder.cl>
+  Copyright (c) 2009 Daniel Molina Wegener <dmw@coder.cl>
 
-    This file is part of pyxser.
+  This file is part of pyxser.
 
-    pyxser is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
+  pyxser is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
 
-    pyxser is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  pyxser is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with pyxser.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with pyxser.  If not, see <http://www.gnu.org/licenses/>.
 
-    <!DOCTYPE pyxs:obj
-              PUBLIC "-//coder.cl//DTD pyxser 1.0//EN"
-              "http://projects.coder.cl/pyxser/dtd/pyxser-1.0.dtd">
- */
+  <!DOCTYPE pyxs:obj
+  PUBLIC "-//coder.cl//DTD pyxser 1.0//EN"
+  "http://projects.coder.cl/pyxser/dtd/pyxser-1.0.dtd">
+*/
 
 #ifndef PYXSER_TOOLS_H
 #define PYXSER_TOOLS_H 1
@@ -39,65 +39,65 @@ extern "C" {
 #endif /* !__cplusplus */
 
 
-extern const PythonTypeSerialize serxConcreteTypes[];
-extern const PythonTypeDeserialize unserxConcreteTypes[];
-extern xmlDtdPtr pyxser_dtd_object;
+    extern const PythonTypeSerialize serxConcreteTypes[];
+    extern const PythonTypeDeserialize unserxConcreteTypes[];
+    extern xmlDtdPtr pyxser_dtd_object;
 
-char *pyxser_GetClassName(PyObject *obj);
+    char *pyxser_GetClassName(PyObject *obj);
 
-xmlNodePtr pyxser_SerializePrimitiveType(PyObject *o, char *name,
-										 PyListObject *dupItems,
-										 xmlDocPtr doc);
+    xmlNodePtr pyxser_SerializePrimitiveType(PyObject *o, char *name,
+                                             PyListObject *dupItems,
+                                             xmlDocPtr doc);
 
-xmlNodePtr pyxser_SerializeXml(PyObject *o, xmlDocPtr *docPtr, xmlNodePtr *rootNode,
-							   xmlNodePtr *currentNode, PyListObject *dupSrcItems);
+    xmlNodePtr pyxser_SerializeXml(PyObject *o, xmlDocPtr *docPtr, xmlNodePtr *rootNode,
+                                   xmlNodePtr *currentNode, PyListObject *dupSrcItems);
 
-xmlNodePtr pyxser_SerializeXmlNonStd(PyObject *o, xmlDocPtr *docPtr,
-									 xmlNodePtr *rootNode, xmlNodePtr *currentNode,
-									 PyListObject *dupSrcItems);
+    xmlNodePtr pyxser_SerializeXmlNonStd(PyObject *o, xmlDocPtr *docPtr,
+                                         xmlNodePtr *rootNode, xmlNodePtr *currentNode,
+                                         PyListObject *dupSrcItems);
 
 
-xmlNodePtr pyxser_RunSerialization(PyObject *item, PyObject *o, PyObject *currentKey,
-								   PyListObject *dupItems, xmlDocPtr *docPtr,
-								   xmlNodePtr *rootNode, xmlNodePtr *currentNode);
+    xmlNodePtr pyxser_RunSerialization(PyObject *item, PyObject *o, PyObject *currentKey,
+                                       PyListObject *dupItems, xmlDocPtr *docPtr,
+                                       xmlNodePtr *rootNode, xmlNodePtr *currentNode);
 
-PyObject *pyxser_UnserializeXml(PythonUnserializationArgumentsPtr obj);
+    PyObject *pyxser_UnserializeXml(PythonUnserializationArgumentsPtr obj);
 
-PyObject *pyxser_SearchModuleType(PyObject *mod, const char *name);
+    PyObject *pyxser_SearchModuleType(PyObject *mod, const char *name);
 
-PyObject *pyxser_GetNonCallableAttributes(PyObject *obj);
+    PyObject *pyxser_GetNonCallableAttributes(PyObject *obj);
 
-int pyxser_PyListContains(PyListObject *lst, PyObject *o);
+    int pyxser_PyListContains(PyListObject *lst, PyObject *o);
 
-xmlNodePtr pyxser_AddReference(PyObject *o, xmlNodePtr currentNode);
+    xmlNodePtr pyxser_AddReference(PyObject *o, xmlNodePtr currentNode);
 
-void pyxser_AddIdentifier(PyObject *o, xmlNodePtr currentNode);
+    void pyxser_AddIdentifier(PyObject *o, xmlNodePtr currentNode);
 
-void pyxser_AddModuleAttr(PyObject *o, xmlNodePtr currentNode);
+    void pyxser_AddModuleAttr(PyObject *o, xmlNodePtr currentNode);
 
-xmlNsPtr pyxser_GetDefaultNs(void);
+    xmlNsPtr pyxser_GetDefaultNs(void);
 
-char *pyxser_ExtractPropertyName(const char *name, xmlNodePtr node);
+    char *pyxser_ExtractPropertyName(const char *name, xmlNodePtr node);
 
-PyObject *pyxser_UnserializeBlock(PythonUnserializationArgumentsPtr obj);
+    PyObject *pyxser_UnserializeBlock(PythonUnserializationArgumentsPtr obj);
 
-int pyxser_ModuleNotMain(const char *mod);
+    int pyxser_ModuleNotMain(const char *mod);
 
-PyObject *pyxser_SearchModule(const char *name);
+    PyObject *pyxser_SearchModule(const char *name);
 
-PyObject *pyxser_CacheModule(PyObject *d, const char *name);
+    PyObject *pyxser_CacheModule(PyObject *d, const char *name);
 
-PyObject *pyxser_SearchObjectInMain(const char *name);
+    PyObject *pyxser_SearchObjectInMain(const char *name);
 
-char *pyxser_GetObjectIdentifier(xmlNodePtr node);
+    char *pyxser_GetObjectIdentifier(xmlNodePtr node);
 
-PyObject *pyxser_CheckAvailableObject(PyObject *dict, char *id);
+    PyObject *pyxser_CheckAvailableObject(PyObject *dict, char *id);
 
-void pyxser_AddAvailableObject(PyObject *dict, char *id, PyObject *o);
+    void pyxser_AddAvailableObject(PyObject *dict, char *id, PyObject *o);
 
-xmlDtdPtr pyxser_GetPyxserDTD(void);
+    xmlDtdPtr pyxser_GetPyxserDTD(void);
 
-int pyxser_ValidateDocument(xmlDocPtr doc);
+    int pyxser_ValidateDocument(xmlDocPtr doc);
 
 #ifdef __cplusplus
 };
