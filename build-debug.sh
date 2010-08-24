5d14e2b9c9d1e7dd05be5bf47afbea9c0dbdc0f0
Daniel Molina Wegener <dmw@coder.cl>
2010-08-24 01:42:04 +0000

    * src/pyxser.c: renewed documentation. * build-debug.sh: renewed build script. * build-normal.sh: renewed build script.

--- build-debug.sh
+++ build-debug.sh
@@ -8,4 +8,3 @@ export CFLAGS='-Wall -Wextra -Wshadow -pedantic -std=c99 -g3 -pg'
 /usr/bin/env python ./setup.py --verbose clean --all
 /usr/bin/env python ./setup.py --verbose build --force --debug
 /usr/bin/env python ./setup.py --verbose install --force
-
