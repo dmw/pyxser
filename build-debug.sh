6b6e660a860f918e6b23805f5ba65c35af253720
Evgeny Cherkashin <eugeneai@irnok.net>
2013-09-13 21:53:07 +0900

    Let's suppose that debug is a development.

--- build-debug.sh
+++ build-debug.sh
@@ -3,9 +3,8 @@
 #
 
 export LC_ALL=C
-export CFLAGS='-Wall -Wextra -Wshadow -pedantic -std=c99 -g3 -pg'
+export CFLAGS='-Wall -Wextra -Wshadow -pedantic -std=c99 -g3 -pg -D PYXSER_DEBUG -O0'
 
 /usr/bin/env python ./setup.py --verbose clean --all 2>&1
 /usr/bin/env python ./setup.py --verbose build --force --debug 2>&1 | sed '/parameter/d'
-/usr/bin/env python ./setup.py --verbose install --force 2>&1
-
+/usr/bin/env python ./setup.py --verbose develop 2>&1
